d1293ea524766648cc6b87bbf463549b4d2336cf
fredykamaulana <fredyka.maulana@gmail.com>
2022-12-01 21:44:53 +0700

    change field entity

--- app/src/main/java/com/fmyapp/githubapp/core/data/localsource/entities/FavouriteUserEntity.kt
+++ app/src/main/java/com/fmyapp/githubapp/core/data/localsource/entities/FavouriteUserEntity.kt
@@ -10,30 +10,15 @@ data class FavouriteUserEntity(
     @ColumnInfo(name = "login")
     val login: String? = null,
 
-    @ColumnInfo(name = "company")
-    val company: String? = null,
-
     @PrimaryKey
     @ColumnInfo(name = "id")
     val id: Int? = null,
 
-    @ColumnInfo(name = "public_repos")
-    val publicRepos: Int? = null,
-
-    @ColumnInfo(name = "followers")
-    val followers: Int? = null,
-
     @ColumnInfo(name = "avatar_url")
     val avatarUrl: String? = null,
 
-    @ColumnInfo(name = "following")
-    val following: Int? = null,
-
     @ColumnInfo(name = "name")
     val name: String? = null,
-
-    @ColumnInfo(name = "location")
-    val location: String? = null,
 
     @ColumnInfo(name = "favourite")
     val favourite: Boolean? = null
